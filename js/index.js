9ca1bf719a41bcecf52e9c09dbc83c877d51a492
Ana Silvera <alsilvera@smu.edu>
2026-05-17 16:11:46 -0500

    Added css and js files and added more html pages

---
+++ js/index.js
@@ -0,0 +1,12 @@
+"use scrict";
+
+(() => {
+
+document.querySelector("#testButton").addEventListener('click', handleClick);
+
+function handleClick() {
+    console.log("Thank you for clicking.") ;
+}
+})();
+
+
