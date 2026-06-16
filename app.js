1580b87218149798b56540a6694047b9ab840f57
Ana Silvera <alsilvera@smu.edu>
2026-06-16 18:07:14 -0500

    Module 6: database integration, responsive cards, random featured project

--- app.js
+++ app.js
@@ -2,31 +2,85 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+import * as db from "./utils/database.js";
+let data = ["Project 1", "Project 2", "Project 3"];
+let projects = [];
 
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
+app.get("/", async (req, res, next) => {
+  try {
+    await db.connect();
+    projects = await db.getAllProjects();
 
-app.use(express.json());
+    // Pick a random project for the featured section
+    const randomIndex = Math.floor(Math.random() * projects.length);
+    const featured = projects[randomIndex];
 
-app.post("/mail", async (req, res) => {   // ending as /mail in my link
+    res.render("index.ejs", {
+      projectArray: projects,
+      featured: featured
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/projects", (req, res) => {
+ res.render("projects.ejs", {projectArray: projects});
+});
+
+app.get("/project/:id", async (req, res, next) => {
+  try {
+    await db.connect();
+    const allProjects = await db.getAllProjects();
+    const id = parseInt(req.params.id, 10);
+    const project = allProjects.find(p => p.id === id);
+
+    if (!project) {
+      throw new Error("No project with that ID");
+    }
+
+    res.render("project.ejs", { project });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/index", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.post("/mail", async (req, res) => {
   await utils
-  .sendMessage(req.body.sub, req.body.txt)
-  .then(() => {
-    res.send({ result: "Message sent!" });
+    .sendMessage(req.body.sub, req.body.txt)
+    .then(() => {
+      res.send({ result: "success" });
     })
     .catch((err) => {
-        console.error("sendMessage failed:", err);
-        res.send({ result: "Message not sent." });
+      console.error("sendMessage failed:", err);
+      res.send({ result: "failure" });
     });
 });
 
 app.listen(port, () => {
-    //console.log(process.env.SENSITIVE_INFO);
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (res.headersSent) return next(err);   // ← add this line
+  let msg = err.message;
+  if (msg !== "No project with that ID") {
+    msg = "There was an internal error. Apologies. We are working on cleaning up the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,39 @@
-(function() {
+
 
 "use strict";
 
+(() => {
+    let userAddress = null;
+let connect = document.querySelector("#wallet-connect");
 
 
-document.querySelector("#contact-form-button").addEventListener("click", submitMail);
+connectWallet();
 
-function submitMail() {
-    console.log("You clicked the submit button.") ;
+connect.addEventListener("click", async () => {
+    connectwallet();
+});
+
+async function connectWallet() {
+  await window.ethereum
+    .request({ method: "eth_requestAccounts" })
+    .then((data) => {
+      userAddress = data[0];
+      let walletString =
+        userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+      connect.innerHTML = walletString;
+      return userAddress;
+    })
+    .catch((err) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log("Please connect a wallet.");
+      } else {
+        console.error(err);
+      }
+    });
 }
+
 })();
 
 
